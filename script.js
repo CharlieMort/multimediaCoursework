93cbfa7a7d6f928f6fa693d159e883b62c9c943c
CharlieMort <61493460+CharlieMort@users.noreply.github.com>
2022-10-20 01:27:35 +0100

    added navbar highlighting also its 1:30 and i have a 9am lab lol

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ setInterval(() => {
     }
     typeText.innerHTML = pauseTimer===0?textz[typeIdx]:textz[typeIdx].substring(0,typeText.innerHTML.length+dir);
 }, delay);
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+let sections = [
+    document.getElementById("about"),
+    document.getElementById("hobbies"),
+    document.getElementById("coursework"),
+    document.getElementById("animation")
+]
+
+let navBarElements = document.getElementsByClassName("navItem")
+
+addEventListener('scroll', (event) => {
+    for (let i = 0; i<sections.length; i++) {
+        if (isInViewport(sections[i])) {
+            navBarElements[i].classList.add("active");
+        }
+        else {
+            navBarElements[i].classList.remove("active");
+        }
+    }
+});
